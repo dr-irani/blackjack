a5391d043f6af13946d074b7e801113b3494d4bf
Darius Irani <dirani2@jhu.edu>
2018-09-21 07:58:42 -0600

    split up play_game method into play_game and setup_game. This separates the initialization of the counter and array to hold hands from the normal gameplay.

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -13,14 +13,22 @@ def shuffle_deck():
     return deck
 
 def deal_hand(int start):
+    hand = [np.list.insert(shuffle_deck[start + card]) for card in range(cn.INITIAL_DEAL_SIZE)]
+    return np.list
 
-def play_game(int num_players):
-    int cards_drawn_counter = 0
-    hands = np.array(num_players)
+def play_game(int start):
     for player in num_players:
         hand = deal_hand(cards_drawn_counter)
+        cards_drawn_counter += cn.INITIAL_DEAL_SIZE
     print(cn.ANOTHER_ROUND_MESSAGE)
+    choice = True
+    if choice:
+        play_game(cards_drawn_counter)
 
+def setup_game(num_players):
+    int cards_drawn_counter = 0
+    hands = np.array(num_players)
+    play_game(cards_drawn_counter)
 
 
 def main():
--- blackjack/constants.py
+++ blackjack/constants.py
@@ -1,6 +1,9 @@
 DECK_SIZE = 52
+INITIAL_DEAL_SIZE = 3
 
 # Strings
+YES = 'y'
+NO = 'n'
 HOW_MANY_PLAYERS_MESSAGE = "How many players would like to play? Please input \
 a number between 1 and 4."
 ANOTHER_ROUND_MESSAGE = "Would you like to play another round? Y/N"
