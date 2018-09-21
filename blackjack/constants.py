34e14c449813ee5e90b7b44f9dbe9fc7737d5afc
Darius Irani <dirani2@jhu.edu>
2018-09-21 09:57:05 -0600

    added str, hit, and deal instance methods to hand class, so these operations can be called on an instantiated hand object in the blackjack game.

--- blackjack/constants.py
+++ blackjack/constants.py
@@ -1,9 +1,8 @@
 DECK_SIZE = 52
 INITIAL_DEAL_SIZE = 3
 FIRST_CARD = 1
-SECOND_CARD = 2
-THIRD_CARD = 3
 SUM = 0
+MAX_SUM = 21
 
 # Strings
 YES = 'y'
@@ -12,9 +11,9 @@ HOW_MANY_PLAYERS_MESSAGE = "How many players would like to play? Please input \
 a number between 1 and 4.\n"
 ONE_PLAYER_LOOK_MESSAGE = "Player %d, it is your turn to be a dealt a hand. \
 All other players please look away from the screen at this time.\n"
-CONTINUE_MESSAGE = "Ready to continue? Y/N\n"
-DISPLAY_HAND_MESSAGE = "Here is your hand: %c, %c, %c. The sum of your cards \
-is %d\n."
+CONTINUE_MESSAGE = "Ready to continue? Press Y when ready.\n"
+DISPLAY_HAND_MESSAGE = "Here is your hand:\n"
+DISPLAY_HAND_SUM = "The sum of your cards is: %d\n."
 ANOTHER_ROUND_MESSAGE = "Would you like to play another round? Y/N\n"
 WINNER_MESSAGE = "The winner is player %d!\n"
 THANKS_FOR_PLAYING_MESSAGE = "Thanks for playing Black Jack, I hope you \
--- blackjack/hand.py
+++ blackjack/hand.py
@@ -2,5 +2,37 @@
 Custom class to define data structure for a Black Jack hand.
 """
 
+import numpy as np
+import constants as cn
+
 class Hand:
-    def __init__(self):
+    def __init__(self, deal_size, deck, start):
+        self.deal_size = deal_size
+        self.deck = deck
+        self.start = start
+        self.hand = self.deal()
+        self.sum = None
+
+    def __str__(self):
+        """
+        TODO: convert this into string for method to return
+        """
+        hand_array = np.array(len(self.hand))
+        [hand_array[pos] = val for val in self.hand]
+        print(DISPLAY_HAND_MESSAGE)
+        for val in hand_array[cn.FIRST_CARD:]:
+            print(val + '\n')
+        printf(DISPLAY_HAND_SUM, hand_array[cn.SUM])
+
+    def hit(self):
+
+    def deal(self):
+        """
+        TODO: fix syntax to be compliable with numpy list data structure.
+        """
+        hand = np.list(cn.INITIAL_DEAL_SIZE)
+        [hand[card + 1] = self.deck[self.start + card] for card in 
+            range(cn.INITIAL_DEAL_SIZE)]
+        hand[0] = sum(hand[1:])
+        return hand
+
